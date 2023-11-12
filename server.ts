aa20fef192d5c5faa63f8b9def57840235d70462
trueno <80685257+truenostyle@users.noreply.github.com>
2023-11-12 18:13:50 +0200

    dobavil handlebars

--- server.ts
+++ server.ts
@@ -1,11 +1,40 @@
-import express from "express";
+import express from 'express';
+import { engine } from 'express-handlebars';
+import { resolve } from 'path';
+
 import { Server } from "socket.io";
 const PORT = 4444;
 
 const app = express();
 
-app.use(express.static("public"));
 
+app.set("view engine", "hbs");
+
+app.engine("hbs", engine({
+  layoutsDir: resolve("views/layouts"),
+  extname: "hbs",
+  defaultLayout: "404"
+}))
+
+app.get("/", (req, res) => {
+    res.render("main", {
+      layout: "index",
+      mainPage: true,
+    });
+  })
+
+  app.get("/room1", (req, res) => {
+    res.render("main", {
+      layout: "index",
+      room1Page: true,
+    });
+  })
+
+  app.get("*", (req, res) => {
+    res.render("main");
+  })
+
+  app.use(express.static("public")); 
 const s = app.listen(PORT, () => {
     console.log(`server at: http://localhost:${PORT}`);
 })
